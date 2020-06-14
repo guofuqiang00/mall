1861aba624096ae5f4e49b589644486b8d234eaa
guofuqiang00 <gfqjava@163.com>
2020-06-14 19:49:03 +0800

    t1

---
+++ src/main/java/com/mall/MallApplication.java
@@ -0,0 +1,14 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}

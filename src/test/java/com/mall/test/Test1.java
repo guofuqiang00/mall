a2da0e3b1724fafeb41b7a494799e26dd7b2afb9
guofuqiang00 <gfqjava@163.com>
2020-06-16 17:00:24 +0800

    t9

--- src/test/java/com/mall/test/Test1.java
+++ src/test/java/com/mall/test/Test1.java
@@ -2,24 +2,34 @@ package com.mall.test;
 import com.mall.MallApplication;
 
 import com.mall.base.BaseCaseTest;
-import com.mall.utils.RedisUtils;
+import com.mall.dao.user.UserMapper;
+import com.mall.entity.User;
+import com.mall.utils.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@ActiveProfiles("dev")
 public class Test1 extends BaseCaseTest {
+    @Autowired(required = false)
+    private UserMapper userMapper;
     @Autowired
-    private RedisUtils redisUtils;
+    RedisUtil redisUtil;
 
     @Test
     public void test(){
+        redisUtil.del("aa");
 
-        String aa = redisUtils.get("aa");
-        System.out.println(aa);
 
     }
 

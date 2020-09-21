cfa99a452842472b978473cf9298922a658e7aef
guofuqiang00 <gfqjava@163.com>
2020-09-21 21:12:01 +0800

    t8

--- src/main/java/com/mall/controller/TestController.java
+++ src/main/java/com/mall/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.mall.controller;
 
+import com.mall.entity.User;
 import com.mall.service.TestService1;
+import com.mall.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -29,5 +33,15 @@ public class TestController {
         String read = testService1.read();
         System.out.println(read);
         return read;
+    }
+
+    @RequestMapping("/get")
+    public R get(){
+        List<User> list = Arrays.asList(
+                new User(1001, "admin", "admin", 18),
+                new User(1002, "root", "root", 22),
+                new User(1003, "123456", "123456", 18)
+        );
+      return  R.ok().put("users",list);
     }
 }
